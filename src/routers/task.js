15f3d55a6b57e1da1ba06b341e0b1bb3f31e9d48
kriti gupta <kritigupta@kritis-MacBook-Air.local>
2024-01-18 17:24:40 +0530

    update

---
+++ src/routers/task.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+
+
+router.post('/tasks', async (req,res) => {
+    const task = new Task(req.body)
+    
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }
+    
+    catch(e) {
+        res.status(400).send(e)   
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }
+    
+    catch(e) {
+        res.status(500).send(e) 
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {   
+    const _id = req.params.id 
+    
+    try{
+        const task = await Task.findById(_id)
+            if(!task) {
+                // when no such id in db
+                return res.status(404).send()
+            }
+            res.send(task)
+    }
+    
+    catch(e){
+        if(e.name === 'CastError'){
+            //for when say id inputed is not of 12 bytes
+            return res.status(400).send('Invalid id')
+        }
+        res.status(500).send(e) 
+    }
+})
+
+
+router.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    const allowedUpdates = ["description", "completed"]
+    const updates = Object.keys(req.body)
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidUpdate) {
+        return res.status(404).send("Invalid updates")
+    }
+
+    try{
+        const task = await Task.findById(_id)
+
+        if (!task){
+            return res.status(404).send('no task with the id exist')
+        }
+
+        //const updatedTask = await Task.findByIdAndUpdate(_id, req.body, {new : true, runValidators: true}) 
+        updates.forEach((update) => task[update] = req.body[update])
+
+        await task.save()
+        
+        res.send(task)
+
+    }
+    catch(e) {
+        res.status(400).send(e) // did not mentioned 500 or server issues
+    }
+})
+
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    try{
+        const task = await Task.findByIdAndDelete(_id)
+        if(!task) {
+            return res.status(404).send('no task with the id exist')
+        }
+        res.send()
+    }
+    catch(e) {
+        res.status(400).send(e) // for when id str is invalid; no where account for 500
+    }
+
+})
+
+
+module.exports = router
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,102 @@
 const express = require('express')
 const router = new express.Router()
+const User = require('../models/user')
 
-router.get('/test', (req,res) => {
-    res.send('from new router')
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+
+    try{
+        await user.save()
+        console.log(user)
+        res.status(201).send(user) // will go into res.body
+    }
+
+    catch(e){
+        res.status(400).send(e) //y hardcode 400, y not 500? could be other error too; y we had to specify ; y 200 was coming when clearly it was an error   
+    }
+})
+
+router.get('/users', async (req, res) => {
+
+    try{
+    const users = await User.find({})
+    res.send(users)
+    }
+
+    catch(e) {
+        res.status(500).send(e) 
+    }
+})
+
+router.get('/users/:id', async (req, res) => {   // : before dynamic values
+    const _id = req.params.id // mongoose automatically converts string id to objct id
+
+    try{
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+
+    catch(e) {
+        if(e.name === 'CastError'){
+            return res.status(400).send('Invalid id')
+        }
+        res.status(500).send(e) 
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    // to communicate error when non-existent fields r updated
+    const allowedUpdates = ["name", "mail", "password","age"]
+    const updates = Object.keys(req.body)
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidUpdate) {
+        return res.status(404).send("Invalid updates")
+    }
+
+    try{
+        const user = await User.findById(_id)
+
+        if (!user){
+            return res.status(404).send('no user with the id exist')
+        }
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+
+        // findByIdAndUpdate bypasses middleware therefore not using it
+        //const updatedUser = await User.findByIdAndUpdate(_id, req.body, {new : true, runValidators: true}) //how would we know it does not check validation, the docs r so poorly written
+        
+        res.send(user)
+
+    }
+    catch(e) {
+        res.status(400).send(e) // did not mentioned 500 or server issues
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    try{
+        const user = await User.findByIdAndDelete(_id)
+        if(!user) {
+            return res.status(404).send('no user with the id exist')
+        }
+        res.send()
+    }
+    catch(e) {
+        res.status(500).send(e) // sends 500 when invalid id !!!
+    }
+
 })
 
 module.exports = router
