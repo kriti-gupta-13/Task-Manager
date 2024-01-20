c047e6be7cb87757940931e0108e27453eb48493
kriti gupta <kritigupta@kritis-MacBook-Air.local>
2024-01-20 20:24:23 +0530

    update

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: { 
@@ -38,21 +39,47 @@ const userSchema = new mongoose.Schema({
         validate(value){
             if(value < 0){
                 throw new Error('age must be positive')
+            } // can add min max too
+        } 
+    },
+    //stores array of tokens
+    tokens : [{
+            token : {
+                type : String,
+                required : true //expired token also part of it ? their can be cases when no active token
+
             }
-        }
-
-        //add min and max or not negative 
-    }
-
-    
+        }]
 })
+
+userSchema.methods.toJSON = function() { //toJSON is called automatically when stringifying the res therefore dont need to explicitly call it anywhere 
+    const user  = this 
+    const userObject = user.toObject() // to remove uneccessary stuff and act on it?
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+
+}  
+
+
+userSchema.methods.generateAuthToken = async function() {   // didnt use arrow fun cuz need to use "this"
+    const user = this
+    const token = jwt.sign({id : user._id.toString()}, 'secret', {expiresIn : '7 days'})
+    
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    
+    return token
+}
 
 userSchema.statics.findByCredentials = async (mail, password) => { // statics is used when function is on model(User.function) whereas schema.method used when function is on specific instance of model (user1.function)
 
     const user = await User.findOne({ mail }) // mail: mail can be written as mail
     
     if(!user) {
-        throw new Error('Unable to login') // where this will be printed - its not in console or res?
+        throw new Error('Unable to login') // where this will be printed - its not in console or res? - throws error to catch; if not defined here then to parent catch
     }
     
     const isMatch = await bcryptjs.compare(password, user.password)
@@ -64,7 +91,7 @@ userSchema.statics.findByCredentials = async (mail, password) => { // statics is
 }
 
 // hashing password before saving
-userSchema.pre('save', async function (next) { // cannot use arrow func cuz does not support this binding
+userSchema.pre('save', async function (next) { // cannot use arrow func cuz does not support "this" binding
     const user = this //refers to document being saved
 
     if (user.isModified('password')) {   // isModified will be true if password created or updated
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../middleware/auth.js')
 const User = require('../models/user')
 
 
@@ -10,8 +11,10 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        const token = await user.generateAuthToken()
+
         console.log(user)
-        res.status(201).send(user) // will go into res.body
+        res.status(201).send({user, token}) // will go into res.body
     }
 
     catch(e){
@@ -22,22 +25,38 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.mail, req.body.password)
-        res.send(user)
-    } catch(e) {
-        res.status(400).send()
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
-
+router.post('/users/logout', auth, async (req, res) => {
     try {
-    const users = await User.find({})
-    res.send(users)
-    }
+        req.user.tokens = req.user.tokens.filter((element) => element.token != req.token)
+        await req.user.save()
+        res.send() // what the use of this?
 
-    catch(e) {
-        res.status(500).send(e) 
+    } catch(e) {
+        res.status(500).send()
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send() 
+
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {   // : before dynamic values
